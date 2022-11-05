4188992bf7c5a31b510bacc16019c5a3e85fd46c
SirojaSB <101753412+SirojaSB@users.noreply.github.com>
2022-11-05 15:17:14 +0700

    Delete backend directory

--- backend/middlewares/errorsHandling.js
+++
@@ -1,9 +0,0 @@
-module.exports = (error, req, res, next) => {
-  const { statusCode = 500, message } = error;
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-
-  next();
-};
